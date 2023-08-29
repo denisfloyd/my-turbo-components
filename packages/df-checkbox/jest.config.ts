9f8022de49537eb9ca62e73bfd1d3d5c2af51e3b
Denis Ladeira <denisladeira1@gmail.com>
2023-08-29 18:23:23 -0300

    feat: checkbox component

---
+++ packages/df-checkbox/jest.config.ts
@@ -0,0 +1,17 @@
+import { Config } from 'jest';
+import { jestConfigJsdom } from 'jest-config-df';
+
+const jestConfig: Config = {
+  ...jestConfigJsdom,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.{tsx,ts,mdx}', '!src/**/index.*'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
+
+module.exports = jestConfig;
---
+++ packages/df-checkbox/src/index.ts
@@ -0,0 +1,5 @@
+import Checkbox from './Checkbox';
+
+import './styles.css';
+
+export default Checkbox;
